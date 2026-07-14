d35eaa72d1e61ba07b52c65d1238f4e0654e876e
Jadeja shaktirajsinh <jadejashaktisinh211@gmail.com>
2026-07-14 18:16:05 +0530

    Time: 1 ms (100%), Space: 47.4 MB (44.06%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,67 +1,29 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        List<Integer> neg = new ArrayList<>();
-        List<Integer> pos = new ArrayList<>();
+        // creat a new arr
+        int[]sorted= new int[nums.length];
 
-        // Separate negative and positive numbers
-        for (int num : nums) {
-            if (num < 0)
-                neg.add(num);
-            else
-                pos.add(num);
-        }
+      // two pointer
+        int left = 0;
+        int right = nums.length-1;
 
-        // Case 1: No negative numbers
-        if (neg.size() == 0) {
-            for (int i = 0; i < pos.size(); i++)
-                pos.set(i, pos.get(i) * pos.get(i));
-            return pos.stream().mapToInt(Integer::intValue).toArray();
-        }
-
-        // Case 2: No positive numbers
-        if (pos.size() == 0) {
-            for (int i = 0; i < neg.size(); i++)
-                neg.set(i, neg.get(i) * neg.get(i));
-            Collections.reverse(neg);
-            return neg.stream().mapToInt(Integer::intValue).toArray();
-        }
-
-        // Case 3: Both negative and positive exist
-        int i = 0, j = 0, id = 0;
-        int n1 = neg.size();
-        int n2 = pos.size();
-        int[] res = new int[n1 + n2];
-
-        // Square negatives and reverse them
-        for (i = 0; i < n1; i++)
-            neg.set(i, neg.get(i) * neg.get(i));
-        Collections.reverse(neg);
-
-        // Square positives
-        for (i = 0; i < n2; i++)
-            pos.set(i, pos.get(i) * pos.get(i));
-
-        // Merge two sorted lists
-        i = 0; // j=0
-        j=0;
-        while (i < n1 && j < n2) {
-            if (neg.get(i) <= pos.get(j)) {
-                res[id++] = neg.get(i++);
-                // res[id]=neg[i]
-                // id++;
-                // i++
-            } else {
-                res[id++] = pos.get(j++);
+       // show the position
+        int pos = nums.length-1;
+      
+      //math.abs = absolute value show 
+        while(left<=right){
+            if(Math.abs(nums[left]) > Math.abs(nums[right])){
+                sorted[pos] = nums[left]*nums[left];
+                left++;
             }
-        } // while loop khatam
+            else{
+                sorted[pos] = nums[right]*nums[right];
+                right--;
+            }
+                    pos--;
 
-        while (i < n1)
-            res[id++] = neg.get(i++);
+        }
+            return sorted;
 
-        while (j < n2)
-            res[id++] = pos.get(j++);
-
-        return res;
     }
 }
