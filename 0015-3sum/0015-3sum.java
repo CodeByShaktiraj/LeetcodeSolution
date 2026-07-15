fb4285d8856209a71c188d47526584111507b078
Jadeja shaktirajsinh <jadejashaktisinh211@gmail.com>
2026-07-15 19:34:59 +0530

    Time: 36 ms (31.28%), Space: 59.2 MB (59.9%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,4 +1,61 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        //sort array
+        Arrays.sort(nums);
 
+        //create a new array list
+        List<List<Integer>> ans =new ArrayList<>();
+
+        //traversion
+        for(int i=0;i<nums.length-2;i++){
+
+            //skip duplicat
+          if(i>0 && nums[i]==nums[i-1])
+          continue;
+
+           //if any no= -7 than -nums[i]=7
+            int target = -nums[i];
+            int left = i+1;
+            int right= nums.length-1;
+
+            while(left<right){
+                 int sum= nums[left] + nums[right];
+                         if(sum ==target){
+                            ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                            left++;
+                            right--;
+
+                            //skip duplicate left side value
+                            while(left<right &&  nums[left]==nums[left-1])
+                                    left++;
+                                   
+                                   while(left<right &&  nums[right]==nums[right+1])
+                                    right--;
+                                   
+                            }
+                            else if(sum>target){
+                                right--;
+                            }
+                            else{
+                                left++;
+                            }
+                         }
+            }
+                                        return ans;
+    }
+}
+
+
+
+
+        
+
+
+    
+
+
+
+/*
         import java.util.*;
 
 class Solution {
@@ -31,3 +88,4 @@ class Solution {
         return res;
     }
 }
+*/
