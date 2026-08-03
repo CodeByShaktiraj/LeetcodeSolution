766fc6a701155b2dc751a4fafc90269b4321129d
Jadeja shaktirajsinh <jadejashaktisinh211@gmail.com>
2026-08-03 22:44:05 +0530

    Time: 16 ms (56.38%), Space: 47 MB (52.14%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+
+class Solution {
+    public String minWindow(String s, String t) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Store frequency of characters in t
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int left = 0;
+        int count = t.length();
+
+        int minLength = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch)) {
+
+                if (map.get(ch) > 0)
+                    count--;
+
+                map.put(ch, map.get(ch) - 1);
+            }
+
+            while (count == 0) {
+
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (map.containsKey(leftChar)) {
+
+                    map.put(leftChar, map.get(leftChar) + 1);
+
+                    if (map.get(leftChar) > 0)
+                        count++;
+                }
+
+                left++;
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE)
+            return "";
+
+        return s.substring(start, start + minLength);
+    }
+}
